34521da91bacb44143ecd140e5c484a507d85cb9
Delvoid <delvoid.dev@gmail.com>
2024-05-13 10:51:26 +0100

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Delvoid/chirpy
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Starting server on port: %s\n", port)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
