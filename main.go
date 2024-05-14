8ce5176aa0552b5bd5a06b0c9653713b1a0a2618
Delvoid <delvoid.dev@gmail.com>
2024-05-14 11:38:12 +0100

    requests made middleware

--- main.go
+++ main.go
@@ -1,28 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+}
+
 func healthzHandlert(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
 func main() {
 	const port = "8080"
 
+	cfg := &apiConfig{}
+
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-
-	assetsDir := http.Dir("./assets")
-	assetsHandler := http.StripPrefix("/app/assets/", http.FileServer(assetsDir))
-	mux.Handle("/app/assets/", assetsHandler)
+	fileServer := http.FileServer(http.Dir("."))
+	appHandler := cfg.middlewareMetricsInc(http.StripPrefix("/app/", fileServer))
+	mux.Handle("/app/", appHandler)
 
 	mux.HandleFunc("/healthz", healthzHandlert)
+
+	mux.HandleFunc("/metrics", cfg.metricsHandler)
+	mux.HandleFunc("/reset", cfg.resetHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
